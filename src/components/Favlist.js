b8464eebdb3295c0ed529f4a9ef167ab483fcb73
shivam kakkar <shivamkakkar4@gmail.com>
2020-01-17 15:30:01 +0530

    Reversing

---
+++ src/components/Favlist.js
@@ -0,0 +1,35 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+
+class Favlist extends Component{
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     favList : []
+  //   }
+  // }
+  render(){
+    return(
+      <div id="shopping">
+        <a href="javascript:void(0)" id="favCloseButton">&times;</a>
+        <h1 id="shoppingTitle">Favourites</h1>
+        <br/>
+        <div id="list">
+          <center>
+            <h5><b>Your favourites list is empty</b></h5>
+          </center>
+          <br/>
+        </div>
+        <div id="listcontent">
+          <ol id="listItem">
+
+          </ol>
+        </div>
+        <center>
+        </center>
+      </div>
+    )
+  }
+}
+
+export default Favlist;
